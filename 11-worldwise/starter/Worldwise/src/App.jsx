5eeedd64bb33861391ec73c95afaf9a87d3d3fe6
Baejita <homokoro@gmail.com>
2024-04-19 22:16:47 +0700

    before Css module

---
+++ 11-worldwise/starter/Worldwise/src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/pricing";
+import Homepage from "./pages/Homepage";
+import PageNotFound from "./pages/PageNotFound";
+function App() {
+  return (
+    <div>
+      {/* <h1>Hello Router!</h1> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="product" element={<Product />} />
+          <Route path="pricing" element={<Pricing />} />
+          <Route path="/" element={<Homepage />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
