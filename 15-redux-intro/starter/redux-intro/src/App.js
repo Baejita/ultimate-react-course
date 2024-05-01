78c590f5feb96775b386d3c9820617595c41d890
Baejita <homokoro@gmail.com>
2024-05-01 23:17:47 +0700

    before redux-thunk

--- 15-redux-intro/starter/redux-intro/src/App.js
+++ 15-redux-intro/starter/redux-intro/src/App.js
@@ -2,15 +2,22 @@ import CreateCustomer from "./features/customers/CreateCustomer.js";
 import Customer from "./features/customers/Customer.js";
 import AccountOperations from "./features/accounts/AccountOperations.js";
 import BalanceDisplay from "./features/accounts/BalanceDisplay.js";
+import { useSelector } from "react-redux";
 
 function App() {
+  const fullName = useSelector((state) => state.customer.fullName); //เพื่อดังข้อมูลชื่อมา สร้างเงื่อไข การแสดงฟอร์ม
   return (
     <div>
       <h1>🏦 The React-Redux Bank ⚛️</h1>
-      <CreateCustomer />
-      <Customer />
-      <AccountOperations />
-      <BalanceDisplay />
+      {fullName === "" ? ( //สร้างเงื่อนไข การแสดงฟอร์มถ้ามีกับไม่มีชื่อ
+        <CreateCustomer />
+      ) : (
+        <>
+          <Customer />
+          <AccountOperations />
+          <BalanceDisplay />
+        </>
+      )}
     </div>
   );
 }
