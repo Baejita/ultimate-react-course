fbb11229a13b5a3273857820f90dc61306988b33
Baejita <homokoro@gmail.com>
2024-05-01 13:33:51 +0700

    afterSpliteFile

--- 15-redux-intro/starter/redux-intro/src/store.js
+++ 15-redux-intro/starter/redux-intro/src/store.js
@@ -1,48 +1,11 @@
-import { createStore } from "redux";
-const initialState = {
-  balance: 0,
-  loan: 0,
-  loanPurpose: "",
-};
-function reducer(state = initialState, action) {
-  switch (action.type) {
-    case "account/deposit":
-      return { ...state, balance: state.balance + action.payload };
-    case "account/withdraw":
-      return { ...state, balance: state.balance - action.payload };
-    case "account/requestLoan":
-      if (state.loan > 0) return state;
-      //Later
-      return {
-        ...state,
-        loan: action.payload.amount,
-        loanPurpose: action.payload.purpose,
-        balance: state.balance + action.payload.amount,
-      };
-    case "account/payLoan":
-      return {
-        ...state,
-        loan: 0,
-        loanPurpose: " ",
-        balance: state.balance - state.loan,
-      };
-    default:
-      return state;
-  }
-}
+import { combineReducers, createStore } from "redux";
+import accountReducer from "./features/accounts/accountSlice";
+import customerReducer from "./features/customers/customerSlice";
 
-const store = createStore(reducer);
-store.dispatch({ type: "account/deposit", payload: 500 });
-console.log(store.getState());
-
-store.dispatch({ type: "account/withdraw", payload: 200 });
-console.log(store.getState());
-
-store.dispatch({
-  type: "account/requestLoan",
-  payload: { amount: 1000, purpose: "Buy a car" },
+const rootReducer = combineReducers({
+  account: accountReducer, // accountคือชื่อที่แสดงใน console เปลี่ยนได้
+  customer: customerReducer,
 });
-console.log(store.getState());
 
-store.dispatch({ type: "account/payLoan" });
-console.log(store.getState());
+const store = createStore(rootReducer);
+export default store;
