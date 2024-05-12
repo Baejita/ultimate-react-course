3ed800551a99d5ea776b55902f7d8f41a30f6da1
Baejita <homokoro@gmail.com>
2024-05-12 19:48:10 +0700

    globalstyled

--- 17-the-wild-oasis/starter/oasis/src/App.jsx
+++ 17-the-wild-oasis/starter/oasis/src/App.jsx
@@ -1,9 +1,47 @@
-import { useState } from "react";
+import styled from "styled-components";
+import GlobalStyle from "./styles/GlobalStyles";
 
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellow;
+`;
+
+const Button = styled.button`
+  font-size: 1.5;
+  padding: 1.2rem 1.6rem;
+  font-weight: 500;
+  border: none;
+  border-radius: var(--border-radius-sm);
+  background-color: var(--color-brand-600);
+  color: var(--color-brand-50);
+
+  box-shadow: var(--shadow-sm);
+  cursor: pointer;
+`;
+
+const Input = styled.input`
+  border: 1px solid var(--color-grey-300);
+  background-color: var(--color-grey-0);
+  border-radius: var(--border-radius-sm);
+  padding: 0.8rem 1.2rem;
+  box-shadow: var(--shadow-sm);
+`;
+
+const StyledApp = styled.main`
+  background-color: orange;
+  padding: 20px;
+`;
 function App() {
   return (
     <>
-      <h1>Hello vite</h1>
+      <GlobalStyle />
+      <StyledApp>
+        <H1>The Wild Oasis</H1>
+        <Button onClick={() => alert("check in")}>Check in</Button>
+        <Button onClick={() => alert("check out")}>Check out</Button>
+        <Input type="number" placeholder="Number of guests"></Input>
+      </StyledApp>
     </>
   );
 }
