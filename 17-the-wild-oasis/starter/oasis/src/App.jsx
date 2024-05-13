9a4b91e02fccb5607f903bbdc238b9241b255caa
Baejita <homokoro@gmail.com>
2024-05-13 16:03:47 +0700

    555

--- 17-the-wild-oasis/starter/oasis/src/App.jsx
+++ 17-the-wild-oasis/starter/oasis/src/App.jsx
@@ -1,32 +1,8 @@
 import styled from "styled-components";
 import GlobalStyle from "./styles/GlobalStyles";
-
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-  background-color: yellow;
-`;
-
-const Button = styled.button`
-  font-size: 1.5;
-  padding: 1.2rem 1.6rem;
-  font-weight: 500;
-  border: none;
-  border-radius: var(--border-radius-sm);
-  background-color: var(--color-brand-600);
-  color: var(--color-brand-50);
-
-  box-shadow: var(--shadow-sm);
-  cursor: pointer;
-`;
-
-const Input = styled.input`
-  border: 1px solid var(--color-grey-300);
-  background-color: var(--color-grey-0);
-  border-radius: var(--border-radius-sm);
-  padding: 0.8rem 1.2rem;
-  box-shadow: var(--shadow-sm);
-`;
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
 
 const StyledApp = styled.main`
   background-color: orange;
@@ -37,7 +13,7 @@ function App() {
     <>
       <GlobalStyle />
       <StyledApp>
-        <H1>The Wild Oasis</H1>
+        <Heading>The Wild Oasis</Heading>
         <Button onClick={() => alert("check in")}>Check in</Button>
         <Button onClick={() => alert("check out")}>Check out</Button>
         <Input type="number" placeholder="Number of guests"></Input>
