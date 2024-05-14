7486e4f5d00c6fc788bdc10f36acf6d8de34fa7d
Baejita <homokoro@gmail.com>
2024-05-14 15:36:12 +0700

    button Primary

--- 17-the-wild-oasis/starter/oasis/src/App.jsx
+++ 17-the-wild-oasis/starter/oasis/src/App.jsx
@@ -3,9 +3,10 @@ import GlobalStyle from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
 import Heading from "./ui/Heading";
+import Row from "./ui/Row";
 
 const StyledApp = styled.main`
-  background-color: orange;
+  /* background-color: orange; */
   padding: 20px;
 `;
 function App() {
@@ -13,10 +14,36 @@ function App() {
     <>
       <GlobalStyle />
       <StyledApp>
-        <Heading>The Wild Oasis</Heading>
-        <Button onClick={() => alert("check in")}>Check in</Button>
-        <Button onClick={() => alert("check out")}>Check out</Button>
-        <Input type="number" placeholder="Number of guests"></Input>
+        <Row>
+          <Row type="horizontal">
+            <Heading type="h1">The Wild Oasis</Heading>
+            <div>
+              <Heading type="h2">Check in and out</Heading>
+              <Button
+                variation="primary"
+                size="medium"
+                onClick={() => alert("check in")}
+              >
+                Check in
+              </Button>
+              <Button
+                variation="secondary"
+                size="large"
+                onClick={() => alert("check out")}
+              >
+                Check out
+              </Button>
+            </div>
+          </Row>
+
+          <Row type="vertical">
+            <Heading type="h3">Form</Heading>
+            <from>
+              <Input type="number" placeholder="Number of guests"></Input>
+              <Input type="number" placeholder="Number of guests"></Input>
+            </from>
+          </Row>
+        </Row>
       </StyledApp>
     </>
   );
