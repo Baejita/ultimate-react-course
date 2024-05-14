53d3cda5b8ec2c635d85f60f11e819eb997b7b77
Baejita <homokoro@gmail.com>
2024-05-14 11:39:24 +0700

    css

--- 17-the-wild-oasis/starter/oasis/src/ui/Heading.jsx
+++ 17-the-wild-oasis/starter/oasis/src/ui/Heading.jsx
@@ -1,10 +1,13 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-const test = `text-align:center`;
+const test = css`
+  text-align: center;
+  ${10 > 5 && "background-color: yellow"}
+`;
 const Heading = styled.h1`
   font-size: 20px;
   font-weight: 600;
-  background-color: yellow;
+
   ${test}
 `;
 
