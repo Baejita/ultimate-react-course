3b35515781f5c6ac6fa6c986d0e4700ec27183d8
Baejita <homokoro@gmail.com>
2024-05-16 22:47:59 +0700

    grid

--- 17-the-wild-oasis/starter/oasis/src/ui/AppLayout.jsx
+++ 17-the-wild-oasis/starter/oasis/src/ui/AppLayout.jsx
@@ -3,20 +3,27 @@ import Header from "./Header";
 import Sidebar from "./Sidebar";
 import styled from "styled-components";
 
+const StyledApplayout = styled.main`
+  display: grid;
+  height: 100vh;
+  grid-template-columns: 26rem 1fr;
+  grid-template-rows: auto 1fr;
+`;
+
 const Main = styled.main`
-  background-color: green;
+  background-color: var(--color-grey-50);
   padding: 4rem 4.8rem 6.4rem;
 `;
 
 function AppLayout() {
   return (
-    <div>
+    <StyledApplayout>
       <Header />
       <Sidebar />
       <Main>
         <Outlet />
       </Main>
-    </div>
+    </StyledApplayout>
   );
 }
 
--- 17-the-wild-oasis/starter/oasis/src/ui/Sidebar.jsx
+++ 17-the-wild-oasis/starter/oasis/src/ui/Sidebar.jsx
@@ -1,9 +1,11 @@
 import styled from "styled-components";
 
 const StyledSidebar = styled.aside`
-  background-color: blue;
+  background-color: var(--color-grey-0);
   padding: 3.2rem 2.4rem;
   border-bottom: 1px solic var(--color-grey-100);
+
+  grid-row: 1 / -1;
 `;
 function Sidebar() {
   return <StyledSidebar>Sidbar</StyledSidebar>;
