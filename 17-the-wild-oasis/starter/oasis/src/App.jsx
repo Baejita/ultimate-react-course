395730c630b8a7aa15696ce3a3ed7b426b17f4b1
Baejita <homokoro@gmail.com>
2024-05-20 20:34:25 +0700

    first Create Form

--- 17-the-wild-oasis/starter/oasis/src/App.jsx
+++ 17-the-wild-oasis/starter/oasis/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "react-hot-toast";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import GlobalStyles from "../src/styles/GlobalStyles";
 import Dashboard from "./pages/Dashboard";
@@ -41,6 +42,27 @@ function App() {
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
+
+      <Toaster
+        position="top-center"
+        gutter={12}
+        containerStyle={{ margin: "8px" }}
+        toastOptions={{
+          success: {
+            duration: 3000,
+          },
+          error: {
+            duration: 5000,
+          },
+          style: {
+            maxWidth: "500px",
+            fontSize: "16px",
+            padding: "16px 24px",
+            backgroundColor: "var(--color-grey-0)",
+            color: "var(--color-grey-700)",
+          },
+        }}
+      />
     </QueryClientProvider>
   );
 }
