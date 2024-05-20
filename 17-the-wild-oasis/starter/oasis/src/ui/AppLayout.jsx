c4b7d0f519ccadc74b74d8f750aead38a1823c3e
Baejita <homokoro@gmail.com>
2024-05-20 23:08:24 +0700

    FormRow

--- 17-the-wild-oasis/starter/oasis/src/ui/AppLayout.jsx
+++ 17-the-wild-oasis/starter/oasis/src/ui/AppLayout.jsx
@@ -13,6 +13,7 @@ const StyledApplayout = styled.main`
 const Main = styled.main`
   background-color: var(--color-grey-50);
   padding: 4rem 4.8rem 6.4rem;
+  overflow: scroll;
 `;
 const Container = styled.div`
   max-width: 120rem;
