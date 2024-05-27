fcf8624641f63c188953ed33e0ccaff5e97f5386
Baejita <homokoro@gmail.com>
2024-05-27 20:06:46 +0700

    booking1

--- 17-the-wild-oasis/starter/oasis/src/features/cabins/CabinTable.jsx
+++ 17-the-wild-oasis/starter/oasis/src/features/cabins/CabinTable.jsx
@@ -4,12 +4,16 @@ import { useCabins } from "./useCabin";
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
 import { useSearchParams } from "react-router-dom";
+import Empty from "../../ui/Empty";
 
 function CabinTable() {
   const { isLoading, cabins } = useCabins();
   const [searchParams] = useSearchParams();
 
   if (isLoading) return <Spinner />;
+  if (!cabins.length) return;
+  <Empty resourceName="cabins" />;
+  //Filter
   const filterValue = searchParams.get("discount") || "all";
 
   let filteredCabins;
@@ -20,6 +24,15 @@ function CabinTable() {
 
   if (filterValue === "with-discount")
     filteredCabins = cabins.filter((cabins) => cabins.discount > 0);
+
+  //Sort by
+  const sortBy = searchParams.get("sortBy") || "startDate-asc;";
+  const [field, direction] = sortBy.split("-");
+  const modifier = direction === "asc" ? 1 : -1;
+  const sortedCabins = filteredCabins.sort(
+    (a, b) => (a[field] - b[field]) * modifier
+  );
+  console.log(modifier, sortedCabins);
 
   return (
     <Menus>
@@ -35,7 +48,8 @@ function CabinTable() {
 
         <Table.Body
           //data={cabins}
-          data={filteredCabins}
+          // data={filteredCabins}
+          data={sortedCabins}
           render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
         />
       </Table>
--- 17-the-wild-oasis/starter/oasis/src/ui/SortBy.jsx
+++ 17-the-wild-oasis/starter/oasis/src/ui/SortBy.jsx
@@ -3,13 +3,21 @@ import Select from "./Select";
 
 function SortBy({ options }) {
   const [serchParams, setSerchParams] = useSearchParams();
+  const sortBy = serchParams.get("sortBy") || "";
 
   function handleChange(e) {
     serchParams.set("sortBy", e.target.value);
     setSerchParams(serchParams);
   }
 
-  return <Select options={options} type="white" onChange={handleChange} />;
+  return (
+    <Select
+      options={options}
+      value={sortBy}
+      type="white"
+      onChange={handleChange}
+    />
+  );
 }
 
 export default SortBy;
